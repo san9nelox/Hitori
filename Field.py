0da5204b658b6404244977b62def018cf5961e51
Pavel Bedin <bedinpasha@mail.ru>
2023-05-08 12:24:05 +0500

    update Field

---
+++ Field.py
@@ -0,0 +1,24 @@
+import random
+
+
+def generate_board(size):
+    board = [[0 for i in range(size)] for j in range(size)]
+    for i in range(size):
+        for j in range(size):
+            board[i][j] = random.randint(1, size)
+    return board
+
+
+def print_board(board):
+    for row in board:
+        print(row)
+
+
+size = int(input("Введите размер поля от 6 до 12: "))
+if size < 6 or size > 12:
+    while size < 6 or size > 12:
+        size = int(input("Введите размер поля от 4 до 12: "))
+
+board = generate_board(size)
+print("Сгенерированное поле:")
+print_board(board)
