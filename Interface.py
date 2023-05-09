85a895a45a184a9c8724761aae63fbd1648261b0
Pavel Bedin <bedinpasha@mail.ru>
2023-05-09 12:16:02 +0500

    fix Interface

--- Interface.py
+++ Interface.py
@@ -3,11 +3,25 @@ from pygame.locals import *
 
 from Field import board, size
 
+# инициализация Pygame
+pygame.init()
+
+# шрифты
+FONT = pygame.font.SysFont("Arial", 30)
+# отображение поля
+cell_size = 50
+
+
+def change_color(color_background, color_number, screen, row, column):
+    cell_value = board[row][column]
+    cell_rect = pygame.Rect(column * cell_size, row * cell_size, cell_size, cell_size)
+    pygame.draw.rect(screen, color_background, cell_rect)
+    text = FONT.render(str(cell_value), True, color_number)
+    text_rect = text.get_rect(center=cell_rect.center)
+    screen.blit(text, text_rect)
+
 
 def game():
-    # инициализация Pygame
-    pygame.init()
-
     # размеры окна
     WINDOW_SIZE = (50 * size, 50 * size)
 
@@ -19,19 +33,9 @@ def game():
     BLACK = (0, 0, 0)
     GRAY = (128, 128, 128)
 
-    # шрифты
-    FONT = pygame.font.SysFont("Arial", 30)
-
-    # отображение поля
-    cell_size = 50
     for i in range(size):
         for j in range(size):
-            cell_value = board[i][j]
-            cell_rect = pygame.Rect(j * cell_size, i * cell_size, cell_size, cell_size)
-            pygame.draw.rect(screen, GRAY, cell_rect)
-            text_surface = FONT.render(str(cell_value), True, BLACK)
-            text_rect = text_surface.get_rect(center=cell_rect.center)
-            screen.blit(text_surface, text_rect)
+            change_color(GRAY, BLACK, screen, i, j)
 
     # обновление экрана
     pygame.display.flip()
@@ -50,22 +54,17 @@ def game():
                     for j in range(size):
                         cell_rect = pygame.Rect(j * cell_size, i * cell_size, cell_size, cell_size)
                         if cell_rect.collidepoint(mouse_pos):
+                            # получаем цвет пикселя в левом верхнем углу ячейки
+                            pixel_color = screen.get_at((j * cell_size, i * cell_size))
                             # если ячейка серая, то перекрашиваем ее в черный
-                            if screen.get_at(cell_rect.center) == pygame.Color(GRAY):
-                                change_color(BLACK, WHITE)
+                            if pixel_color == GRAY:
+                                change_color(BLACK, WHITE, screen, i, j)
                             # если ячейка черная, то перекрашиваем ее в белый
-                            elif screen.get_at(cell_rect.center) == pygame.Color(BLACK):
-                                change_color(WHITE, BLACK)
+                            elif pixel_color == BLACK:
+                                change_color(WHITE, BLACK, screen, i, j)
                             # если ячейка белая, то перекрашиваем ее в серый
-                            elif screen.get_at(cell_rect.center) == pygame.Color(WHITE):
-                                change_color(GRAY, BLACK)
-
-        def change_color(color_background, color_number):
-            cell_value = board[i][j]
-            pygame.draw.rect(screen, color_background, cell_rect)
-            text = FONT.render(str(cell_value), True, color_number)
-            text_rect = text.get_rect(center=cell_rect.center)
-            screen.blit(text, text_rect)
+                            elif pixel_color == WHITE:
+                                change_color(GRAY, BLACK, screen, i, j)
 
         # обновление экрана
         pygame.display.flip()
