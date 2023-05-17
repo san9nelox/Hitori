5dfdda7a3917ba92df9d05b8a5a9434ed4c9cb13
Pavel Bedin <bedinpasha@mail.ru>
2023-05-17 15:28:08 +0500

    main can choose the mode

--- hotori_logic/main.py
+++ hotori_logic/main.py
@@ -1,4 +1,37 @@
-from interface.GUI import game
+import tkinter as tk
+
+
+def main():
+    root = tk.Tk()
+    root.geometry("300x250+800+200")
+
+    root.title("Выберите режим")
+
+    def choose_cli():
+        root.destroy()
+        cli()
+
+    def choose_gui():
+        root.destroy()
+        gui()
+
+    button_cli = tk.Button(root, text="Режим отладки", command=choose_cli)
+    button_cli.pack()
+
+    button_gui = tk.Button(root, text="Режим игры", command=choose_gui)
+    button_gui.pack()
+
+    root.mainloop()
+
+
+def gui():
+    from interface.GUI import game
+    game()
+
+
+def cli():
+    print("Вы cli")
+
 
 if __name__ == "__main__":
-    game()
+    main()
