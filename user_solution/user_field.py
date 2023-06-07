fe8c85ad9624dd2f14d7d690d586dd657ec0fb6d
san9nelox <bedinpasha@mail.ru>
2023-06-07 14:00:18 +0500

    Blank to check

---
+++ user_solution/user_field.py
@@ -0,0 +1,17 @@
+from hitori_logic.find_solution import hitori_solution, return_board, check_connected_white
+
+hitori_solution()
+board = return_board()
+board_colors = [['g'] * len(board[0]) for _ in range(len(board))]
+
+
+def check_duplicates_in_row():
+    pass
+
+
+def check_duplicates_in_column():
+    pass
+
+
+def check_horizontal_neigh():
+    pass
